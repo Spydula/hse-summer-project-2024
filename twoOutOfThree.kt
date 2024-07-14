7faa2f0f0a523fcce8b3c70bf553a7ec77f8e569
Spydula <gubanovgeorge300@gmail.com>
2024-07-15 00:41:46 +0500

    fix

--- twoOutOfThree.kt
+++ twoOutOfThree.kt
@@ -1,43 +1,17 @@
 class Solution {
     fun twoOutOfThree(nums1: IntArray, nums2: IntArray, nums3: IntArray): List<Int> {
-        var in1 = mutableMapOf<Int, Int>()
-        var in2 = mutableMapOf<Int, Int>()
-        for(index in nums1.indices)
-        {
-            in2[nums1[index]] = (in2[nums1[index]] ?: 0) + 1
-            if(in2[nums1[index]] == 1)
-            {
-                in1[nums1[index]] = (in1[nums1[index]] ?: 0) + 1
-            }
-        }
-        in2.clear()
-        for(index in nums2.indices)
-        {
-            in2[nums2[index]] = (in2[nums2[index]] ?: 0) + 1
-            if(in2[nums2[index]] == 1)
-            {
-                in1[nums2[index]] = (in1[nums2[index]] ?: 0) + 1
-            }
-        }
-        in2.clear()
-        for(index in nums3.indices)
-        {
-            in2[nums3[index]] = (in2[nums3[index]] ?: 0) + 1
-            if(in2[nums3[index]] == 1)
-            {
-                in1[nums3[index]] = (in1[nums3[index]] ?: 0) + 1
-            }
-        }
-        var answer = mutableListOf<Int>()
-        for(key in in1.keys)
-        {
-            val value = in1[key]
-            if (value != null && value >= 2)
-            {
-                answer.add(key)
-            }
-        }
-        return answer
+        var set_nums1 = nums1.toSet()
+        var set_nums2 = nums2.toSet()
+        var set_nums3 = nums3.toSet()
+
+        var set_nums1_and_nums2 = set_nums1 intersect set_nums2
+        var set_nums2_and_nums3 = set_nums2 intersect set_nums3
+        var set_nums1_and_nums3 = set_nums1 intersect set_nums3
+ 
+        set_nums1_and_nums2 = set_nums1_and_nums2 union set_nums2_and_nums3
+        var answer = set_nums1_and_nums2 union set_nums1_and_nums3        
+        
+        return answer.toList()
 
     }
 }
