1624993a19bebeda115f13a58d38823b3fc7809e
Spydula <gubanovgeorge300@gmail.com>
2024-07-17 17:52:11 +0500

    fix fix

--- twoOutOfThree.kt
+++ twoOutOfThree.kt
@@ -1,17 +1,14 @@
 class Solution {
     fun twoOutOfThree(nums1: IntArray, nums2: IntArray, nums3: IntArray): List<Int> {
-        var set_nums1 = nums1.toSet()
-        var set_nums2 = nums2.toSet()
-        var set_nums3 = nums3.toSet()
+        var set1 = nums1.toSet()
+        var set2 = nums2.toSet()
+        var set3 = nums3.toSet()
 
-        var set_nums1_and_nums2 = set_nums1 intersect set_nums2
-        var set_nums2_and_nums3 = set_nums2 intersect set_nums3
-        var set_nums1_and_nums3 = set_nums1 intersect set_nums3
- 
-        set_nums1_and_nums2 = set_nums1_and_nums2 union set_nums2_and_nums3
-        var answer = set_nums1_and_nums2 union set_nums1_and_nums3        
+        var set12 = set1 intersect set2
+        var set23 = set2 intersect set3
+        var set13 = set1 intersect set3      
         
-        return answer.toList()
+        return (set12 union set23 union set13).toList()
 
     }
 }
