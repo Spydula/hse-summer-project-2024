b56a22eb598d35c1a1f4fd918053e732ffc9ca6f
Spydula <gubanovgeorge300@gmail.com>
2024-07-17 17:39:08 +0500

    fix fix

--- zeroarray.kt
+++ zeroarray.kt
@@ -1,22 +1,7 @@
 class Solution {
-    fun non_zero_numbers(nums: IntArray): Int {
-        var amount = 0
-        for(index in nums.indices)
-        {
-            if(nums[index] != 0)
-            {
-                amount++
-            }
-        }
-        return amount
-    }
     fun minimumOperations(nums: IntArray): Int {
-        if(non_zero_numbers(nums) == 0)
-        {
-            return 0
-        }
         var count = 0
-        while(non_zero_numbers(nums) != 0)
+        while(nums.count{ x -> x > 0} != 0)
         {
             var positive_min = nums.filter {x -> x > 0}.min()
             for(index in nums.indices)
