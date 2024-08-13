b68936b6ce43a1878853b479090084ec5bcda3da
Spydula <gubanovgeorge300@gmail.com>
2024-08-13 19:29:40 +0500

    last fix

--- points.kt
+++ points.kt
@@ -1,14 +1,9 @@
 class Solution {
-    fun count_time(x1: Int, y1: Int, x2: Int, y2: Int): Int
-    {
-        return max(max(x1, x2) - min(x1, x2), max(y1, y2) - min(y1, y2))
-    }
     fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
-        var total_time = 0;
-        for(i in 1..points.size-1)
-        {
-            total_time += count_time(points[i-1][0], points[i-1][1], points[i][0], points[i][1])
+        var time = 0;
+        for(i in 1..points.size-1) {
+            time += max(abs(points[i - 1][0] - points[i][0]), abs(points[i - 1][1] - points[i][1]))
         }
-        return total_time
+        return time
     }
 }
